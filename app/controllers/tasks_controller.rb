281d7a0001fc017591a878e12ea09496fff0e748
Juris Roganovs <juris.roganovs@francity.com>
2020-06-09 22:55:41 +0300

    Added create new tasks method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,24 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
