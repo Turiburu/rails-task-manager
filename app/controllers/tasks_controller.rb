0d5f9c10e463eb56889a4d0c328e30b4e91530af
Juris Roganovs <juris.roganovs@francity.com>
2020-06-10 02:43:46 +0300

    final commit for Tasks list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,6 +29,13 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
